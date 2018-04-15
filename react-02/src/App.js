b1ef6f1bbdc8fddface39c2147a885904672fd62
eyekorea <eyekorea0@gmail.com>
2018-04-15 19:01:35 +0900

    react 의 메서드가 실행되는 순서를 체크 해 볼 수 있다.

---
+++ react-02/src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+export default class App extends Component{
+    // 마운트
+    constructor(props){
+        super(props);
+        console.log('constructor');
+    };
+    componentWillMount(){
+        console.log('componentWillMount');
+    };
+    componentDidMount(){
+        console.log('componentDidMount');
+    };
+
+    // 변경
+    componentWillReceiveProps(){
+        console.log('componentWillReceiveProps');
+    };
+    shouldComponentUpdate(nextProps, nextState){
+        console.log('shouldComponentUpdate');
+        return true;
+    };
+    componentWillUpdate(){
+        console.log('componentWillUpdate');
+    };
+    componentDidUpdate(){
+        console.log('componentDidUpdate');
+    };
+
+    // 언마운트
+    componentWillUnmount(){
+        console.log('componentWillUnmount');
+    };
+
+    render(){
+        console.log('render');
+        const setStateHandler = (e)=>{
+            console.log(' * call setState() ');
+            this.setState({r:Math.random()});
+        };
+        return (
+            <div>
+                <button onClick={setStateHandler}>setState</button>
+            </div>
+        );
+    };
+};
---
+++ react-02/src/main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import style from "./index.css";
+import style2 from "./indexLess.less";
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
