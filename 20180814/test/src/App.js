c05e7d25076cd3d1829faaa144c59a37ea8ac2c3
eyekorea <eyekorea0@gmail.com>
2018-08-14 18:17:07 +0900

    공부중..

---
+++ 20180814/test/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import Movie from './movie';
+
+class App extends Component {
+  componentWillMount() {
+    
+  }
+  componentDidMount() {
+    
+  }
+  render() {
+    return (
+      <div className="App">
+        <Movie />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ 20180814/test/src/movie.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './movie.css';
+
+class Movie extends Component{
+
+  render(){
+    return (
+      <h1>hellow this is movie</h1>
+    )
+  }
+}
+export default Movie;
